b606c01fe80da6bd68c65346b7bab8bd1720250f
DanielRM23 <DanielRM@ciencias.unam.mx>
2024-05-19 11:49:20 -0600

    Borro cosas innecesarias y actualizo carpeta Cuestionario

--- CalculadoraSencilla/prueba.java
+++
@@ -1,9 +0,0 @@
-public class prueba{
-
-    public static void main(String[] args) {
-     
-        calculadora calcu = new calculadora(false, "esta es la marca", 0, "este es el modelo");
-        System.out.println(calcu.sumar(0, 0));
-        System.out.println(calcu.fact2(5));
-    }
-}
